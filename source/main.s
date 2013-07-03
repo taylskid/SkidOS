5bd8b80d9480f62e084e0e9b77e5007a2df7f741
SirSkidmore <sirskidmore@gmail.com>
2013-07-03 16:12:00 -0400

    function-y goodness

--- source/main.s
+++ source/main.s
@@ -2,33 +2,35 @@
 .globl _start
 _start:
 
-        .section .text
+b main
 
-        ldr r0,=0x20200000
+.section .text
+main:
+mov sp,#0x8000
 
-        mov r1,#1
-        lsl r1,#18
-        str r1,[r0,#4]
+mov r0,#16
+mov r1,#1
+bl SetGpioFunction
 
 loop$:
-        mov r1,#1
-        lsl r1,#16
-        str r1,[r0,#40]
+    mov r0,#16
+    mov r1,#0
+    bl SetGpio
 
-        mov r2,#0x3F0000
-wait1$:
-        sub r2,#1
-        cmp r2,#0
-        bne wait1$
+    mov r0,#0x3F0000
+    wait1$:
+            sub r0,#1
+            cmp r0,#0
+            bne wait1$
 
-        mov r1,#1
-        lsl r1,#16
-        str r1,[r0,#28]
+    mov r0,#16
+    mov r1,#1
+    bl SetGpio
 
-        mov r2,#0x3F0000
-wait2$:
-        sub r2,#1
-        cmp r2,#0
-        bne wait2$
+    mov r0,#0x3F0000
+    wait2$:
+            sub r0,#1
+            cmp r0,#0
+            bne wait2$
 
-        b loop$
+    b loop$
